6fb4bee9ad121a612e84ffb74fad4b85b3326d8c
justin <ju3tin@hotmail.co.uk>
2018-08-22 14:42:25 +0100

    please work

--- index.php
+++ index.php
@@ -1,70 +1,17 @@
 <?php
-/*
- * Copyright IBM Corp. 2016
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * @package WordPress
  */
 
- /**
-  * This PHP file uses the Slim Framework to construct a REST API.
-  * See Cloudant.php for the database functionality
-  */
-require 'vendor/autoload.php';
-require_once('./Cloudant.php');
-$app = new \Slim\Slim();
-$dotenv = new Dotenv\Dotenv(__DIR__);
-try {
-  $dotenv->load();
-} catch (Exception $e) {
-    error_log("No .env file found");
- }
-$app->get('/', function () {
-  global $app;
-    $app->render('index.html');
-});
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-$app->get('/api/visitors', function () {
-  global $app;
-  $app->contentType('application/json');
-  $visitors = array();
-  if(Cloudant::Instance()->isConnected()) {
-    $visitors = Cloudant::Instance()->get();
-  }
-  echo json_encode($visitors);
-});
-
-$app->post('/api/visitors', function() {
-  global $app;
-  $app->contentType('application/json');
-  $visitor = json_decode($app->request()->getBody(), true);
-  if(Cloudant::Instance()->isConnected()) {
-    $doc = Cloudant::Instance()->post($visitor);
-    echo json_encode($doc);
-  } else {
-    echo json_encode($visitor);
-  }
-});
-
-$app->delete('/api/visitors/:id', function($id) {
-	global $app;
-	Cloudant::Instance()->delete($id);
-    $app->response()->status(204);
-});
-
-$app->put('/api/visitors/:id', function($id) {
-	global $app;
-	$visitor = json_decode($app->request()->getBody(), true);
-    echo json_encode(Cloudant::Instance()->put($id, $visitor));
-});
-
-$app->run();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
